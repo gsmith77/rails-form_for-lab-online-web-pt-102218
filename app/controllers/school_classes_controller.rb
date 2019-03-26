c07712324a687911f6038b090a5a7af2eb254a86
grant smith <grantsmith1256@gmail.com>
2019-03-26 20:36:20 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -10,7 +10,7 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-    @school_class = SchoolClass.create(params.require(:school_class).permit(:title, :room_number))
+    @school_class = SchoolClass.create(params.require(:school_classes).permit(:title, :room_number))
     redirect_to school_class_path(@school_class)
   end
 
